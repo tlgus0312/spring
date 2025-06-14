a83a8a19a2df858f24f3e16fbc1bdf1aca86ac7a
sihyeon <tlgus0312@naver.com>
2025-06-14 21:33:09 +0900

    settings

---
+++ src/test/java/com/sihe/spring/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sihe.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
