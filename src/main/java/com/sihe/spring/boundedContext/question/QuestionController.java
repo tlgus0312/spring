fb1aa372e7ad9fe1b96c04a4bf7b3cca2d95d366
sihyeon <tlgus0312@naver.com>
2025-06-16 11:01:59 +0900

    서비스에 getQuestion 메서드 구현

--- src/main/java/com/sihe/spring/boundedContext/question/QuestionController.java
+++ src/main/java/com/sihe/spring/boundedContext/question/QuestionController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import java.util.List;
 @Controller
 @RequiredArgsConstructor
-public class questionController {
+public class QuestionController {
     private final QuestionService questionService;
  @GetMapping("/question/list")
     public String list(Model model) {
@@ -20,6 +20,9 @@ public class questionController {
  }
     @GetMapping(value = "/question/detail/{id}")
     public String detail(Model model, @PathVariable("id") Integer id) {
-        return "question_detail";
+     Question question = questionService.getQuestion(id);
+
+     model.addAttribute("question",question);
+     return "question_detail";
     }
 }
--- src/main/java/com/sihe/spring/boundedContext/question/QuestionService.java
+++ src/main/java/com/sihe/spring/boundedContext/question/QuestionService.java
@@ -1,17 +1,28 @@
 package com.sihe.spring.boundedContext.question;
 
+import com.sihe.spring.service.exception.DataNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class QuestionService {
 
     private final QuestionRepository questionRepository;
-    public List<Question> findAll(){
+
+    public List<Question> findAll() {
         return questionRepository.findAll();
     }
 
+    public Question getQuestion  (Integer id) throws DataNotFoundException {
+        Optional<Question> question = questionRepository.findById(id);
+        if (question.isEmpty()) throw new DataNotFoundException("question not found");
+
+            return question.get();
+
+    }
 }
+
