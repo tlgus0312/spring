67a3c1e9e21920d662609b691095248f2eb8d7f8
sihyeon <tlgus0312@naver.com>
2025-06-16 09:47:12 +0900

    질문 상세 컨트롤러 만들기

---
+++ src/main/java/com/sihe/spring/boundedContext/question/QuestionService.java
@@ -0,0 +1,17 @@
+package com.sihe.spring.boundedContext.question;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class QuestionService {
+
+    private final QuestionRepository questionRepository;
+    public List<Question> findAll(){
+        return questionRepository.findAll();
+    }
+
+}
--- src/main/java/com/sihe/spring/boundedContext/question/questionController.java
+++ src/main/java/com/sihe/spring/boundedContext/question/questionController.java
@@ -1,21 +1,25 @@
 package com.sihe.spring.boundedContext.question;
 
-import org.springframework.ui.Model;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
-
 @Controller
 @RequiredArgsConstructor
 public class questionController {
-    private final QuestionRepository questionRepository;
+    private final QuestionService questionService;
  @GetMapping("/question/list")
     public String list(Model model) {
-     List<Question> questionList= questionRepository.findAll();
+     List<Question> questionList= questionService.findAll();
 
      model.addAttribute("questionList",questionList);
      return "question_list";
  }
+    @GetMapping(value = "/question/detail/{id}")
+    public String detail(Model model, @PathVariable("id") Integer id) {
+        return "question_detail";
+    }
 }
